090352665eb234b0840f079eb643cff752c5641d
pratamaandiko <pratamandiko@gmail.com>
2021-10-19 13:46:53 +0700

    minimax kurang objective

--- src/ai/minimax.py
+++ src/ai/minimax.py
@@ -5,7 +5,8 @@ from time import time
 from src.model.piece import Piece
 
 from src.model import Board
-from src.constant import ShapeConstant
+from src.constant import ColorConstant, ShapeConstant
+
 from src.model import State
 
 from typing import Tuple, List
@@ -22,24 +23,34 @@ class Minimax:
     
     def drop_piece(self,board,column):
         #fungsi untuk menjatuhkan piece
-        pass
+         for row in range(board.row - 1, -1, -1):
+            if board[row, column].shape == ShapeConstant.BLANK:
+                piece = Piece(ShapeConstant.CIRCLE, ColorConstant.BLUE)
+                board.set_piece(row, column, piece)
+                return 0
+
 
     def MinimaxAlghorithm(self,board : Board,depth : int,maximizing : bool):
         if (depth == 0) :
-            return [None,eval(board)]
+            score = random.randint(0,board.col)
+            return [None,score]
         
-        if (maximizing):
+       if (maximizing):
             value = -inf 
             for column in range(board.col):
-                new_board = board.copy()
+                new_board = deepcopy(board)
                 self.drop_piece(new_board,column)
+                print("\n\n board baru {0} depth {1} maximize \n\n".format(column,depth))
+                print(new_board)
                 self.MinimaxAlghorithm(new_board,depth-1,False)
 
         else : #minimizing
             value = inf
             for column in range(board.col):
-                new_board = board.copy()
+                new_board = deepcopy(board)
                 self.drop_piece(new_board,column)
+                print("\n\n board baru {0} depth {1} minimize \n\n".format(column,depth))
+                print(new_board)
                 self.MinimaxAlghorithm(new_board,depth-1,True)
                 
 
@@ -50,5 +61,6 @@ class Minimax:
         piece = Piece(state.players[n_player].shape,state.players[n_player].color)
         # best_movement = (random.randint(0, state.board.col), random.choice([ShapeConstant.CROSS, ShapeConstant.CIRCLE])) #minimax algorithm
         best_movement = (0, piece.shape) #minimax algorithm
+        self.MinimaxAlghorithm(state.board,2,True)
         return best_movement
     
